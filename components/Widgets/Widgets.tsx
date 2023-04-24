ae44d617c5a2bea179287465fedd4523cf6dc15f
Anthony Fink <40363736+psytonik@users.noreply.github.com>
2023-04-24 09:24:47 +0300

    working on the Widgets, Search component

--- components/Widgets/Widgets.tsx
+++ components/Widgets/Widgets.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import {MagnifyingGlassIcon} from "@heroicons/react/24/outline";
 
 const Widgets = () => {
 	return (
-		<div>
-			Widget
+		<div className="xl:w-[600px] hidden lg:inline ml-8 space-y-5">
+			<div className="w-[90%] xl:w-[75%] sticky top-0 bg-white py-1.5 z-50">
+				<div className="flex items-center border rounded-full p-3 bg-gray-200 relative">
+					<MagnifyingGlassIcon className="h-5 z-50 text-gray-500 "/>
+					<input type="text" className="border-none rounded-full focus:ring-0 bg-gray-100 inset-0 absolute pl-11 border-gray-500 text-gray-700 focus:shadow-lg focus:bg-white" placeholder="Search Tweeter"/>
+				</div>
+			</div>
 		</div>
 	);
 };
