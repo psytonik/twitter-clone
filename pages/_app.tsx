ca57b18eacc99120b84d3a1be80e4ee247790f50
Anthony Fink <40363736+psytonik@users.noreply.github.com>
2023-04-25 20:07:10 +0300

    adding firebase kit and next-auth firebase config wrap application with next-auth adding auth method in server

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,10 @@
 import '@/styles/globals.css'
 import type {AppProps} from 'next/app'
+import { SessionProvider } from "next-auth/react"
 
-const App = ({Component, pageProps}: AppProps) => {
-	return <Component {...pageProps} />
+const App = ({Component, pageProps:{session, ...pageProps}}: AppProps) => {
+	return <SessionProvider session={session}>
+		<Component {...pageProps} />
+	</SessionProvider>
 }
 export default App;
