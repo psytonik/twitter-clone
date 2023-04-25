07c6b8633e132cf8772611ccba3a9809d467bedf
Anthony Fink <40363736+psytonik@users.noreply.github.com>
2023-04-25 20:43:41 +0300

    fix

--- pages/api/auth/[...nextAuth].ts
+++ pages/api/auth/[...nextAuth].ts
@@ -1,13 +1,17 @@
 import NextAuth from "next-auth"
 import GoogleProvider from "next-auth/providers/google";
 
-export const authOptions = {
+
+export const authOptions:any = {
 	// Configure one or more authentication providers
 	providers: [
 		GoogleProvider({
-			clientId: process.env.GOOGLE_CLIENT_ID,
-			clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+			clientId: process.env.GOOGLE_CLIENT_ID as string,
+			clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
 		})
 	],
+	pages:{
+		signin:'/auth/signin'
+	}
 }
 export default NextAuth(authOptions)
